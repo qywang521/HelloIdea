90551076bad7d31926bca4089690bf05a57bf427
qywang521 <qywang521@126.com>
2017-09-25 07:24:58 +0800

    test git

--- src/wz/HelloIdea.java
+++ src/wz/HelloIdea.java
@@ -3,6 +3,6 @@ package wz;
 public class HelloIdea {
     public static void main(String[] args) {
         System.out.println("Hello Idea!");
-
+        System.out.println("git commit!");
     }
 }
